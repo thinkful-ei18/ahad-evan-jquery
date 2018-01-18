3b41910b0dd770be62a370b27580480771591675
Evan Garrett <evang522@gmail.com>
2018-01-18 16:53:33 -0500

    Finished Fizzbuzz

--- Fizz-Buzz/index.js
+++ Fizz-Buzz/index.js
@@ -1,3 +1,5 @@
+'use strict';
+
 function fizzBuzz(countTo) {
   
   const result = [];
@@ -10,18 +12,35 @@ function fizzBuzz(countTo) {
     } else if (i % 3 === 0) {
       result.push('fizz');
     } else {
-     
       result.push(i);
     }
   }
   return result;
 }
 
-$("form").on("submit",event=>{
+
+let pushToHTML = (data) => {
+  data.forEach((arrayMember)=> {
+    if (arrayMember === 'fizz') {
+      $('.js-results').append('<div class="fizz-buzz-item fizz"><span>fizz</span></div>');
+    } else if (arrayMember === 'buzz') {
+      $('.js-results').append('<div class="fizz-buzz-item buzz"><span>buzz</span></div>');
+    } else if (arrayMember === 'fizzbuzz') {
+      $('.js-results').append('<div class="fizz-buzz-item fizzbuzz"><span>fizzbuzz</span></div>');
+    } else {
+      $('.js-results').append(`<div class="fizz-buzz-item"><span>${arrayMember}</span></div>`);
+    }
+  });
+};
+
+let listen = () => {
+  $('form').on('submit', (event) => {
     event.preventDefault();
-    let val=$("input").val();
-    
-    fizzBuzz(val);
-});
+    let val=$('input').val();
+    let data = fizzBuzz(val);
+    pushToHTML(data);
+  });
+};
 
 
+listen();
