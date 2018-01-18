75fa65978970d412b37da29ad04b7433c8d142c5
Ahad Darkhosh <darkhosh@hotmail.com>
2018-01-18 12:48:32 -0800

    Fizz-Buzz

---
+++ Fizz-Buzz/index.js
@@ -0,0 +1,29 @@
+function fizzBuzz(countTo) {
+  
+//   const result = [];
+  
+  for (let i = 1; i <= countTo; i++) {
+    if (i % 15 === 0) {
+      result.push('fizzbuzz');
+    } else if (i % 5 === 0) {
+      result.push('buzz');
+    } else if (i % 3 === 0) {
+      result.push('fizz');
+    } else {
+       
+      $(".js-results").append("<div class='fizz-buzz-item'><span>1</span></div>");
+
+    //   result.push(i);
+    }
+  }
+//   return result;
+}
+
+$("button").on("click",event=>{
+    event.preventDefault();
+    let val=$("input").val();
+    
+    fizzBuzz(val);
+});
+
+
